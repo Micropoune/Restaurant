0517f02cc1ddd03651ef8d8c40758e1384a868ab
SB <beaulieu.sylvie.2.0@gmail.com>
2018-12-04 16:48:31 -0500

    Connexion Base de Donnees

---
+++ Restaurant/RestoConceptDataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Restaurant
+{
+	internal class RConceptDataContext
+	{
+		private object connectionString;
+
+		public RConceptDataContext(object connectionString)
+		{
+			this.connectionString = connectionString;
+		}
+
+		internal void SubmitChanges()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
