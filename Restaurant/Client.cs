3e74697be5f078f1a909a9e338c117aaa5156c6a
Mathieu Moisan <mathieu_moisan@hotmail.com>
2018-12-04 17:18:19 -0500

    ajustement

---
+++ Restaurant/Client.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Restaurant
+{
+    public class Client : Utilisateur
+    {
+        private string m_Email;
+        private string m_Adresse;
+        private string m_Telephone;
+
+        public string getEmail()
+        {
+            return this.m_Email;
+        }
+
+        public void setEmail(string p_Email)
+        {
+            this.m_Email = p_Email;
+        }
+
+        public string getAdresse()
+        {
+            return this.m_Adresse;
+        }
+
+        public string getTelephone()
+        {
+            return this.m_Telephone;
+        }
+
+        public void setTelephone(string p_Telephone)
+        {
+            this.m_Telephone = p_Telephone;  
+        }
+    }
+}
