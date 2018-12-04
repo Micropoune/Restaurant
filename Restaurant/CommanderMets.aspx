865718add3c86c81ac6fa2b3fa351a8fb9bfa9fa
Alpo <alwadimahabi@gmail.com>
2018-12-04 18:05:28 -0500

    CommanderMets Debut

---
+++ Restaurant/CommanderMets.aspx
@@ -0,0 +1,130 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/PageMaitre.Master" AutoEventWireup="true" CodeBehind="CommanderMets.aspx.cs" Inherits="Restaurant.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    
+
+    <style type="text/css">
+        .auto-style1 {
+            width: 121px;
+        }
+        .auto-style2 {
+            margin-left: 42px;
+        }
+        .auto-style3 {
+            margin-top: 18px;
+        }
+        .auto-style4 {
+            width: 232px;
+        }
+    </style>
+    
+
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="Contenu" runat="server">
+    <div>
+
+    
+    <div style="width: 60%; float:left">
+        <div>
+             <table style="width:100%" style="margin-left:50px">
+                   
+  <tr>
+    <td class="auto-style1"><asp:Image ID="Image1" runat="server"   ImageUrl="C:/Users/MAIN/source/repos/Micropoune/Restaurant/pizza.jpg" Height="100px" Width="100px" />
+             </td>
+    <td>
+        <asp:Panel ID="Panel1" runat="server" GroupingText="Pizza" Width="565px">
+            <br />
+            <table style="width:100%">
+                <tr>
+                    <td>
+                        <asp:Label ID="Label1" runat="server" Text="Format" style="margin-left:50px"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:RadioButton ID="rdb_petite" runat="server" Text=" Petite 8&quot" />
+                    </td>
+                    <td>
+                        <asp:RadioButton ID="rdb_grande" runat="server" Text=" Grande 12&quot"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+
+                    </td>
+                    <td>
+                        <asp:RadioButton ID="rdb_moyenne" runat="server" Text=" Moyenne 10&quot"/>
+                    </td>
+                    <td>
+                         <asp:RadioButton ID="rdb_large" runat="server" Text=" X-Large 16&quot"/>
+                    </td>
+                </tr>
+
+            </table>
+            
+            
+            
+
+            <br />
+            
+           
+            <br />
+            <br />
+                </asp:Panel>
+    </td>
+    
+  </tr>
+  <tr>
+    <td class="auto-style1"></td>
+    <td>
+         <asp:Panel ID="Panel2" runat="server" GroupingText="Boisson" Width="565px">
+            <table style="width:100%">
+                <tr>
+                    <td>
+                        <asp:DropDownList ID="ddl_choix_boisson" runat="server"></asp:DropDownList>
+                    </td>
+                    <td>
+                        <asp:RadioButton ID="rdb_350ml" runat="server" Text="350 ml" />
+                    </td>
+                    <td>
+                        <asp:DropDownList ID="ddl_quantite" runat="server"></asp:DropDownList>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <asp:RadioButton ID="rdb_2litre" runat="server" Text="2 Litre" />
+                    </td>
+                    <td>
+                        <asp:Button ID="btn_ajouter" runat="server" CssClass="auto-style2" Text="Ajouter" />
+                    </td>
+                </tr>
+            </table>
+                </asp:Panel>
+    </td>
+   
+  </tr>
+</table> 
+               
+            
+        </div>
+    </div>
+            <div style="float:right">
+                <asp:LinqDataSource ID="LinqDataSource1" runat="server"></asp:LinqDataSource>
+                <asp:Panel ID="Panel3" runat="server" Height="772px" Width="318px" style="margin: 0px 50px 50px 50px" CssClass="auto-style3" >
+
+                    <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+
+
+                    <table>
+                        <tr>
+                            <td class="auto-style4">
+                                <asp:Button ID="btn_retour" runat="server" Text="Retour" />
+                            </td>
+                            <td>
+                                <asp:Button ID="btn_valider" runat="server" Text="Valider" />
+                            </td>
+                        </tr>
+                    </table>
+                </asp:Panel>
+            </div>
+    
+  </div>
+</asp:Content>
---
+++ Restaurant/CommanderMets.aspx.designer.cs
@@ -0,0 +1,177 @@
+﻿//------------------------------------------------------------------------------
+// <généré automatiquement>
+//     Ce code a été généré par un outil.
+//
+//     Les modifications apportées à ce fichier peuvent provoquer un comportement incorrect et seront perdues si
+//     le code est régénéré.
+// </généré automatiquement>
+//------------------------------------------------------------------------------
+
+namespace Restaurant {
+    
+    
+    public partial class WebForm1 {
+        
+        /// <summary>
+        /// Contrôle Image1.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image1;
+        
+        /// <summary>
+        /// Contrôle Panel1.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel Panel1;
+        
+        /// <summary>
+        /// Contrôle Label1.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+        
+        /// <summary>
+        /// Contrôle rdb_petite.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton rdb_petite;
+        
+        /// <summary>
+        /// Contrôle rdb_grande.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton rdb_grande;
+        
+        /// <summary>
+        /// Contrôle rdb_moyenne.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton rdb_moyenne;
+        
+        /// <summary>
+        /// Contrôle rdb_large.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton rdb_large;
+        
+        /// <summary>
+        /// Contrôle Panel2.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel Panel2;
+        
+        /// <summary>
+        /// Contrôle ddl_choix_boisson.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddl_choix_boisson;
+        
+        /// <summary>
+        /// Contrôle rdb_350ml.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton rdb_350ml;
+        
+        /// <summary>
+        /// Contrôle ddl_quantite.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddl_quantite;
+        
+        /// <summary>
+        /// Contrôle rdb_2litre.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton rdb_2litre;
+        
+        /// <summary>
+        /// Contrôle btn_ajouter.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_ajouter;
+        
+        /// <summary>
+        /// Contrôle LinqDataSource1.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinqDataSource LinqDataSource1;
+        
+        /// <summary>
+        /// Contrôle Panel3.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel Panel3;
+        
+        /// <summary>
+        /// Contrôle GridView1.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView1;
+        
+        /// <summary>
+        /// Contrôle btn_retour.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_retour;
+        
+        /// <summary>
+        /// Contrôle btn_valider.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_valider;
+    }
+}
